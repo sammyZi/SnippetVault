feb837f5f411a6c0c0e5253dd89bfa7275c6b0e0
SammyZi <bhingesamarth@gmail.com>
2026-03-11 17:00:54 +0530

    feat: add get_user_id_by_email function to Database interface with user_email argument

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -171,7 +171,12 @@ export interface Database {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      get_user_id_by_email: {
+        Args: {
+          user_email: string
+        }
+        Returns: string
+      }
     }
     Enums: {
       [_ in never]: never
